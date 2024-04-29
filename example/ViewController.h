a82599f925bed296f9004c063a6e1761f0bcd49c
matsune <yuma.matsune@gmail.com>
2024-04-29 11:44:21 +0900

    init

---
+++ example/ViewController.h
@@ -0,0 +1,8 @@
+#import <Cocoa/Cocoa.h>
+#import <SceneKit/SceneKit.h>
+
+@interface ViewController : NSViewController
+
+@property(weak) IBOutlet SCNView *scnView;
+
+@end
---
+++ example/ViewController.mm
@@ -0,0 +1,19 @@
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+  [super setRepresentedObject:representedObject];
+
+  // Update the view, if already loaded.
+}
+
+@end
